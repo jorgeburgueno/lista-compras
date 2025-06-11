100516c6b7be7f04d12383de57719de26e85ba9b
jorgeburgueno <jorge.albert16@hotmail.com>
2025-06-11 11:32:06 -0700

    fix: recipe menu and detail states now use unique IDs

--- src/components/DisplayRecipe.jsx
+++ src/components/DisplayRecipe.jsx
@@ -2,22 +2,39 @@ import React from "react";
 import { useState } from "react";
 
 function DisplayRecipe({ recipes, deleteRecipe }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [details, setDetails] = useState(false);
+  const [isOpen, setIsOpen] = useState(null);
+  const [details, setDetails] = useState(null);
 
   return (
     <div>
       {recipes.map((recipe) => (
-        <div key={recipe.id} onClick={() => setDetails(!details)}>
+        <div
+          key={recipe.id}
+          onClick={() =>
+            details !== recipe.id ? setDetails(recipe.id) : setDetails(null)
+          }
+        >
           {recipe.name}
-          <button onClick={() => setIsOpen(!isOpen)}>⋮</button>
-          {isOpen && ( // if isOpen is true open the buttons
+          <button
+            onClick={() =>
+              isOpen !== recipe.id ? setIsOpen(recipe.id) : setIsOpen(null)
+            }
+          >
+            ⋮
+          </button>
+          {isOpen === recipe.id && ( // if isOpen is true open the buttons
             <div>
               <button>edit</button>
               <button onClick={() => deleteRecipe(recipe.id)}>X</button>{" "}
             </div>
           )}
-          {details && <div>{recipe.ingredients}</div>}
+          {details === recipe.id && (
+            <div>
+              {recipe.ingredients.map((ingredient, index) => (
+                <div key={index}>{ingredient}</div>
+              ))}
+            </div>
+          )}
         </div>
       ))}
     </div>
